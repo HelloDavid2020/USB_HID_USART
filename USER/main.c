43fafd7af37c3c860bb7bfeac58ab2bab93c942c
zhaodawei <zhaodawei@mobike.com>
2016-08-14 11:45:04 +0800

    no message

--- USER/main.c
+++ USER/main.c
@@ -131,20 +131,30 @@ get_device_value(&biaotou[2],100);
 
 	 while(1) //
 	 {
-		if (USB_ReceiveFlg == TRUE) //收到后上位机的数据后，将1S发送一次数据给PC
-		{				
-			GPIO_Toggle(GPIOB, GPIO_Pin_3|GPIO_Pin_4|GPIO_Pin_5);
+//		if (USB_ReceiveFlg == TRUE) //收到后上位机的数据后，将1S发送一次数据给PC
+//		{				
+//			GPIO_Toggle(GPIOB, GPIO_Pin_3|GPIO_Pin_4|GPIO_Pin_5);
 
-//			printf("\r\nUSB 收到数据:");
-//			
-//			for(i=0;i<ReceiveLength;i++)
-//			{
-//				printf("%c",Receive_Buffer[i]);
-//			}
-			sprintf((void*)tx_buf_data,"source_add: <%02d> <%02d> <%02d> <%02d>",source_add[0],source_add[1],source_add[2],source_add[3]);
-			USB_SendString(tx_buf_data);
-			USB_ReceiveFlg = 0x00;
-		}
+////			printf("\r\nUSB 收到数据:");
+////			
+////			for(i=0;i<ReceiveLength;i++)
+////			{
+////				printf("%c",Receive_Buffer[i]);
+////			}
+//			sprintf((void*)tx_buf_data,"source_add: <%02d> <%02d> <%02d> <%02d>",source_add[0],source_add[1],source_add[2],source_add[3]);
+//			USB_SendString(tx_buf_data);
+//			USB_ReceiveFlg = 0x00;
+//		}
+		 
+		 
+		get_device_value(&biaotou[0],100);
+		get_device_value(&biaotou[1],100);
+		get_device_value(&biaotou[2],100); 
+		get_device_value(&biaotou[3],100); 
+
+		GPIO_Toggle(GPIOB, GPIO_Pin_3|GPIO_Pin_4|GPIO_Pin_5);
+		delay_ms(200);
+
 	 }
 }
 void USB_SendString(u8 *str)
